21a24b7a161eda45b7c16c34fafbae5bd7b7f480
LeonAppDev <liangren64@gmail.com>
2017-04-21 10:15:27 +1200

    Update Program.cs

--- Company/LICA/Program.cs
+++ Company/LICA/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,79 +8,7 @@ namespace Practice
 {
     public class Program
     {
-        public int Summation(int[] numbers)
-        {
-            int sum = 0;
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                sum += numbers[i];
-            }
-
-            return sum;
-
-        }
-
-
-        public string[] combination(string original)
-        {
-            int count = 0;
-            List<string> returnArray = new List<string>();
-
-            //while (count < original.Length)
-            //{
-            //Recursion could start only when string length >1, deal with condition original lenght e1  
-            if (count == (original.Length - 1))
-                returnArray.Add(original[count].ToString());
-            else
-            {
-                char c = original[count];
-                string[] subArray = combination(original.Substring(count + 1, original.Length - count - 1));
-
-                returnArray.Add(c.ToString());
-                foreach (string s in subArray)
-                {
-                    returnArray.Add(s);
-                    returnArray.Add(c + s);
-                }
-            }
-            //    count++;
-
-
-            //}
-            return returnArray.ToArray();
-
-
-        }
-
-
-        public string[] Anagram(string original)
-        {
-            int count = 0;
-            List<string> anagramList = new List<string>();
-
-            if (1 == original.Length)
-                return new string[] { original };
-            else
-            {
-                char c = original[count];
-
-                string[] returnArray = Anagram(original.Substring(count + 1, original.Length - count - 1));
-
-                foreach (string s in returnArray)
-                {
-                    for (int i = 0; i < s.Length; i++)
-                    { string temp = s.Insert(i, c.ToString());
-                        anagramList.Add(temp);
-                    }
-                    anagramList.Add(s + c);
-                 
-
-                }
-            }
-
-            return anagramList.ToArray();
-        }
-
+      
 
         /*This function take a string as input and return true if this string is palindrome 
          * or the anagram of this string is palindrome. 
@@ -125,100 +53,10 @@ namespace Practice
         static void Main(string[] args)
         {
 
-            //int count = Convert.ToInt32(System.Console.ReadLine());
-            //int[] numbers = new int[count];
-
-            //if (count > 0 && count < 100)
-            //{
-            //    for (int i = 0; i < count; i++)
-            //    {
-            //        numbers[i] = Convert.ToInt32(System.Console.ReadLine());
-            //    }
-                
-            //}
-
-            Program p = new Program();
-
-            //System.Console.WriteLine("The result of calculation of {0}", p.Summation(numbers));
-
-       
-
-            string input = System.Console.ReadLine();
-
-            //string[] output = p.combination(input);
-
-            string[] output = p.Anagram(input);
-
-            foreach (string s in output)
-            {
-                if (s[0].Equals(s[s.Length - 1]) && s[1].Equals(s[s.Length - 2]) && s[2].Equals(s[s.Length - 3]) && s[3].Equals(s[s.Length - 5]))
-                    System.Console.WriteLine(s);
-            }
-
-            System.Console.ReadLine();
+          
         }
     }
 
-
-interface Shape
-    {
-        int getArea();
-
-    }
-
-    class Rectangle : Shape
-    {
-
-        private int _height;
-        private int _width;
-        public Rectangle(int height, int width)
-        {
-            _height = height;
-            _width = width;
-        }
-
-        public int getArea()
-        {
-            return _height * _width;
-        }
-
-    }
-
-    class Triangle : Shape
-    {
-
-        private int _height;
-        private int _width;
-        public Triangle(int height, int width)
-        {
-            _height = height;
-            _width = width;
-        }
-
-        public int getArea()
-        {
-            return (int)((_height * _width) / 2);
-        }
-
-    }
-
-    class Circle : Shape
-    {
-
-        private int _radius;
-
-        public Circle(int radius)
-        {
-            _radius = radius;
-
-        }
-
-        public int getArea()
-        {
-            return (int)3.14 * _radius * _radius;
-        }
-
-    }
 
 
 }
